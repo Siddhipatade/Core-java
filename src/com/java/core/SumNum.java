27098e2dd8087365f11d2c7f8070f2178980d8e4
Siddhi Patade <patadesiddhi20@gmail.com>
2024-01-01 23:58:13 +0530

    3

---
+++ src/com/java/core/SumNum.java
@@ -0,0 +1,26 @@
+package com.java.core;
+
+import java.util.Scanner;
+
+public class SumNum {
+
+    public static void main(String[] args) {
+        // TODO Auto-generated method stub
+        // sum of two numbers
+
+        Scanner s = new Scanner(System.in);
+        System.out.println("Enter the two numbers");
+
+        System.out.println("Enter the first number");
+        int num1 = s.nextInt();
+
+        System.out.println("Enter the second number");
+        int num2 = s.nextInt();
+
+        int sum = num1 + num2;
+        System.out.println("Sum of the two numbers: " + sum);
+
+        s.close();
+    }
+
+}

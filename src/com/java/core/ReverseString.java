166eaecba1ae848e1cf23b3d8ace0075eafae1c2
Siddhi Patade <patadesiddhi20@gmail.com>
2024-01-02 23:04:17 +0530

    1

--- src/com/java/core/ReverseString.java
+++ src/com/java/core/ReverseString.java
@@ -3,7 +3,8 @@ package com.java.core;
 public class ReverseString {
 
 	public static void main(String[] args) {
-		// Reverse a String
+		// TODO Auto-generated method stub
+		// reverse string
 		String original = "Hello, World!";
         String reversed = new StringBuilder(original).reverse().toString();
         System.out.println("Original: " + original);
